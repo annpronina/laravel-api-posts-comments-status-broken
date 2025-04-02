a6f6476a452ee86b9a70e1e7da6fef499ee0d73b
unknown <your@email.example>
2025-04-02 14:26:58 +0300

    role model, migration, controller added

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,8 @@ class User extends Authenticatable
     public function comments() {
         return $this->hasMany(Comment::class);
     }
+
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
 }
